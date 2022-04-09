88ec51ecf0f748cb352c081c5c2ee0e7d0e7d15d
Marcelo <cello.ma16@gmail.com>
2022-04-09 10:50:43 -0300

    adiciona decoração nas cartas (front e back), cria funcoes para setar altura e largura do corpo, cria efeitos de flipcard

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 let seqCartas = [];
 
+let selecCartas = [];
+
 function setupInicial() {
-    let qtdCartas = prompt("Olá, jogador! Vamos jogar cartas? O objetivo deste jogo é acertar o par de cartas repetidas em uma mesma jogada. Para começar, informe quantidade de cartas com as quais você deseja jogar. Lembrando que só será aceito um número PAR de 4 a 14.");
-    if (qtdCartas >= 4 && qtdCartas <= 14 && (qtdCartas % 2 == 0)) { //o jogo acontece!
-        distribuirImagens(qtdCartas);
-    }
+    let qtdCartas = -1;
+    do {
+        qtdCartas = prompt("Olá, jogador! Vamos jogar cartas? O objetivo deste jogo é acertar o par de cartas repetidas em uma mesma jogada. Para começar, informe quantidade de cartas com as quais você deseja jogar. Lembrando que só será aceito um número PAR de 4 a 14.");
+        qtdCartas = Number.parseInt(qtdCartas);
+        //o jogo acontece!
+    } while (qtdCartas < 4 || qtdCartas > 14 || (qtdCartas % 2 != 0));
+    distribuirImagens(qtdCartas);
+    atribuirLarguraAltura(qtdCartas);
+}
+
+function atribuirLarguraAltura(qtdCartas) {
+    const cardPorLinha = qtdCartas / 2;
+    atribuirLargura(cardPorLinha);
+    atribuirAltura();
+}
+
+function atribuirLargura(cardPorLinha) {
+    //a largura de cada carta = 137px (117 + 10px pra cada lado horizontal)
+    const larguraMaxima = cardPorLinha * 137;
+    const element = document.querySelector(".cards");
+    element.style.maxWidth = `${larguraMaxima}px`;
+    element.style.minWidth = `${137}px`;
+    console.log("Low");
+}
+
+function atribuirAltura() {
+    //a altura de cada carta = 166px (146px + 10px pra cada lado verticalmente)
+    const alturaMinima = 166 * 2;
+    const element = document.querySelector(".cards");
+    element.style.minHeight = `${alturaMinima}px`;
 }
 
 function distribuirImagens(qtdCartas) {
@@ -28,3 +56,13 @@ function randomizarPosicoes(vetor) {
             [seqCartas[indiceAleatorio], seqCartas[contadorRegressivo]];
     }
 }
+
+function armazenarDadosClick(divClicada) {
+    const element = divClicada.querySelector(".imagem-waifu");
+    selecCartas.push(element);
+}
+
+function limparClicaveis() {
+    selecCartas.
+}
+
